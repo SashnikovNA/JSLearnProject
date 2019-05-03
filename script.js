b1ca0c74e930008ebfcd5f3e0f797c89018ce966
KapodastR <kapodastr@inbox.ru>
2019-05-04 00:54:00 +0300

    firstTestCommit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+console.log(4/0);
+console.log('string'*9);
+
+// comment
+/*
+2312
+23123
+eqwe
+*/
+
+let person = {
+    name:'John',
+    age: 25,
+    isMarried: false
+};
+console.log(person.name);
+console.log(person['name']);
+
+let arr =  ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+// alert ("Hello World!");
+
+//let answer = confirm('Are you here?');
+
+//console.log (answer);
+
+/*
+let answer = +prompt('Are you  18?', '');
+
+console.log (typeof(answer));
+*/
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(!isChecked || isClose);
