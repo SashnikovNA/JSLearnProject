ff2d97b9bcbfde1f97afe800568341c54eb33405
sashnikov <nikita.sashnikov@natlex.ru>
2019-05-08 19:05:41 +0300

    прошел урок ветвления

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var money = +prompt("Ваш бюджет на месяц?"),
+/* var money = +prompt("Ваш бюджет на месяц?"),
     time = prompt('Введите дату в формате YYYY-MM-DD'),
     item1ExpName = prompt('Введите обязательную статью расходов №1 в этом месяце'),
     item1ExpCost = +prompt('Во сколько обойдутся траты по ней?'),
@@ -25,7 +25,7 @@ let andy = 3;
 
 
 
-/*  Ответы на вопросы из задания:
+  Ответы на вопросы из задания:
 1 - Сколько типов данных существует в JS?
 в JS существет 6 типов данных:
 - number ('infinity' and 'NaN'  also there)
@@ -41,3 +41,38 @@ should use the command : 'console.log();
 3 - Какая функция операторов || и &&? 
 General function - logical compearing (AND - &&, OR - ||);
 */
+
+let num = 50;
+/*
+if (num<49) {
+    console.log("not right!!!");
+} else if (num > 100) {
+    console.log("much more!!!");
+} else {
+    console.log("Correct!!!");
+};
+
+(num == 50) ? console.log("correct"): console.log("uncorrect");
+*/
+switch (num) {
+    case num < 49: 
+        console.log("uncorrect");
+        break;
+    case num > 100:
+        console.log("much more!!!");
+        break;
+    case num > 80:
+        console.log("even much more!!!");
+        break;
+    case 50:
+        console.log("good!");
+        break;
+    default:
+        console.log("something strange!!!");
+        break;
+}
+
+
+
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,30 @@
-let arr =  ['plum.png','orange.jpg','apple.bmp'];
 
-console.log(arr[0]);
+let num = 50;
+/*
+if (num<49) {
+    console.log("not right!!!");
+} else if (num > 100) {
+    console.log("much more!!!");
+} else {
+    console.log("Correct!!!");
+};
+
+(num == 50) ? console.log("correct"): console.log("uncorrect");
+*/
+switch (num) {
+    case num < 49: 
+        console.log("uncorrect");
+        break;
+    case num > 100:
+        console.log("much more!!!");
+        break;
+    case num > 80:
+        console.log("even much more!!!");
+        break;
+    case 50:
+        console.log("good!");
+        break;
+    default:
+        console.log("something strange!!!");
+        break;
+}
