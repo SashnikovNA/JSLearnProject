6875f9564c48da872d0db8506ba4dafdbbe26f90
KapodastR <kapodastr@inbox.ru>
2019-05-26 00:57:19 +0300

    OOP + arr + obj lesson

--- scriptWithExample.js
+++ scriptWithExample.js
@@ -1,5 +1,27 @@
 // var a = console.log([ ] + false - null + true);
-let x = [ ] + 1 + 2 + 3+4;
-a = console.log(0 || "" || 2 || undefined || true || falsе );
+let arr = ["first", 2, 3, "four", 5];
+arr.forEach(function(item, i, mass) {
+    console.log(i + ': ' + item + " (массив: " + mass + ")");
+})
+console.log(arr);
 
-b = alert(  "Ёжик" > "яблоко" );
+
+let mass = [1, 3, 4, 6, 7];
+for (let key of mass) {
+    console.log(key);
+}
+
+
+let soldier = {
+    health: 400,
+    armor: 100
+};
+
+let john = {
+    health: 100
+};
+
+john.__proto__ = soldier;
+
+console.log(john);
+console.log(john.armor);
